f5b1c4a495d6341129389ffed2f5630889d00dc4
mariamawitseifu <merrybee34@gmail.com>
2026-07-02 16:38:32 +0300

    fix

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,6 +5,10 @@ const bcrypt = require("bcryptjs");
 const User = sequelize.define(
   "User",
   {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
